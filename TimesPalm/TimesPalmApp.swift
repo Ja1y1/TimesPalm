2bca6d5dd3f938968228a00b986d8e34299ffa31
Ja1y1 <146494173+Ja1y1@users.noreply.github.com>
2023-12-20 17:07:34 +0300

    Initial Commit

---
+++ TimesPalm/TimesPalmApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimesPalmApp.swift
+//  TimesPalm
+//
+//  Created by JAY on 20/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct TimesPalmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
