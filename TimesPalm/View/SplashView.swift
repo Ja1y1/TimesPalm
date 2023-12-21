4441631193003673314e49556026b7d70f23c9d2
Ja1y1 <146494173+Ja1y1@users.noreply.github.com>
2023-12-21 15:10:32 +0300

    mapView splashView

---
+++ TimesPalm/View/SplashView.swift
@@ -0,0 +1,18 @@
+//
+//  SplashView.swift
+//  TimesPalm
+//
+//  Created by JAY on 20/12/2023.
+//
+
+import SwiftUI
+
+struct SplashView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SplashView()
+}
