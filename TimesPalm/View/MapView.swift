630d8b747ee673866d5238053920289602a41426
Ja1y1 <146494173+Ja1y1@users.noreply.github.com>
2023-12-21 15:11:34 +0300

    splash view

--- TimesPalm/View/MapView.swift
+++ TimesPalm/View/MapView.swift
@@ -9,7 +9,32 @@ import SwiftUI
 
 struct MapView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+
+            Rectangle()
+                .fill(Color.mapcolor)
+                .ignoresSafeArea()
+            
+            Image("Map")
+                .resizable()
+                .scaledToFit()
+                .frame(width:987, height:651)
+//          Image("2")
+//                .resizable()
+//                .scaledToFit()
+//                .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/)
+//            
+//            Image("3")
+//                  .resizable()
+//                  .scaledToFit()
+//                  .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/)
+//             
+//           
+            
+            
+           
+        }
+       
     }
 }
 
--- TimesPalm/View/SplashView.swift
+++ TimesPalm/View/SplashView.swift
@@ -8,9 +8,41 @@
 import SwiftUI
 
 struct SplashView: View {
+    @State var isActive: Bool = false
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+        ZStack{
+            if self.isActive{
+                //MapView()
+            }
+                else{
+                Rectangle()
+                Color.splashbkg
+                    .ignoresSafeArea()
+                
+                Image("Logo")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width:451, height:494)
+//                    
+//                    VStack{
+//                        Text("Times Palm")
+//                            .font(.largeTitle)
+//                            .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+//                            .frame(width: 308, height:78)
+//                    }.padding(.top, 500.0)
+               
+            }
+            
+        }
+        
+        .onAppear{
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1.0){
+                withAnimation{
+                self.isActive = true
+                }
+            }
+            
+        }    }
 }
 
 #Preview {
