bc66605ec68de874c463f894ba8dc9d5dba6e342
WajdAlhadlaq <86900714+WajdAlhadlaq@users.noreply.github.com>
2023-12-23 18:35:14 +0300

    Modify Onboarding pages
    Swipe from right to left (arabic)
    modify the position of (التالي) button left bottom

--- TimesPalm/View/OnBoardingView.swift
+++ TimesPalm/View/OnBoardingView.swift
@@ -28,13 +28,16 @@ struct OnboardingPageView: View {
                 .position(x:-1000, y:1300)
                 .scaleEffect(0.19) // Move scaleEffect after position
 
-            Text(onboardingPage.description)
+           /* Text(onboardingPage.description)
+                .padding()
                 .font(.system(size:32))
                 .foregroundColor(.oBtext)
                 .frame(width: 500, height: 156)
                 .background(Color.onboarding) // Set the background color here
                 .cornerRadius(20)
-                .position(x: 1000, y: 250)
+                .position(x: 950, y: 250)
+                .multilineTextAlignment(.center)
+             */
 
             VStack {
                 
@@ -49,20 +52,30 @@ struct OnboardingPageView: View {
                         .resizable()
                         .aspectRatio(contentMode: .fit)
                         .frame(width: 500, height: 500)
-                    
-                    
+                
                     
                     Image(onboardingPage.correct)
                         .resizable()
                         .aspectRatio(contentMode: .fit)
                         .frame(width: 464, height: 283)
+                    
+                    
+                    Text(onboardingPage.description)
+                        .padding()
+                        .font(.system(size:32))
+                        .foregroundColor(.oBtext)
+                        .frame(width: 500, height: 156)
+                        .background(Color.onboarding) // Set the background color here
+                        .cornerRadius(20)
+                        .position(x: 900, y: 80)
+                        .multilineTextAlignment(.center)
 
                     
                 }
                 Text("تحقق")
                 .font(.system(size: 40))
                 .padding()
-                .frame(width: 164, height: 104)
+                .frame(width: 164, height:104)
                 .foregroundColor(.white)
                 .background(Color.button)
                 .cornerRadius(20)
@@ -74,51 +87,74 @@ struct OnboardingPageView: View {
 
 struct OnboardingView: View {
     @State var currentPage = 0
-
+    
     let onboardingPages = [
         OnboardingPage(palmImage: "Palmwithdates", basketImage: "Basket", description: "سؤال من جدول الضرب المراد حله",equation:" 2 x 2 = ?",correct: ""),
         OnboardingPage(palmImage: "Palmwithdates2", basketImage: "Basket", description: "اجمع حبات التمر في السلة لحل المعادلة والتحقق من الجواب",equation:" 2 x 2 = ?",correct: ""),
         OnboardingPage(palmImage: "Palmwithdates3", basketImage: "Basketwithdates", description: "ظهور الحل والإجابة الصحيحة بعد التحقق",equation:" 2 x 2 = 4",correct: "Correct"),
     ]
-
+    
     
     var body: some View {
-        VStack {
-            TabView(selection: $currentPage) {
-                ForEach(0..<3) { index in
-                    OnboardingPageView(onboardingPage: onboardingPages[index])
-                        .tag(index)
+        
+        ZStack{
+            VStack {
+                TabView(selection: $currentPage) {
+                    ForEach((0..<onboardingPages.count).reversed(), id: \.self) { index in
+                        OnboardingPageView(onboardingPage: onboardingPages[index])
+                            .tag(index)
+                    }
                 }
-            }
-            .tabViewStyle(PageTabViewStyle(indexDisplayMode: .always))
-            
-            .background(
+                .tabViewStyle(PageTabViewStyle(indexDisplayMode: .always))
+                
+                
+                
+                
+                .background(
                     Image("OnboardingBG")
                         .resizable()
                         .aspectRatio(contentMode: .fill)
                         .edgesIgnoringSafeArea(.all)
                         .frame(width: 1000,height: 1000)
-                        
+                    
                 )
+                
+                Spacer()
+                
+                
+                HStack {
+                    
+                    Spacer()
+                    Button(action: {
+                        withAnimation {
+                            if currentPage < onboardingPages.count - 1 {
+                                currentPage += 1
+                            } else {
+                                // Handle completion or navigate to the main app
+                            }
+                        }
+                    }) {
+                        Text(currentPage < onboardingPages.count - 1 ? "التالي" : "ابدأ اللعب")
+                            .frame(width:180, height:50)
+                            .foregroundColor(.white)
+                            .font(.system(size:30))
+                            .background(
+                             RoundedRectangle(cornerRadius: 10)
+                             .fill(Color.next)
+                            )
+                               }
+                    .padding(.trailing,960)
+                    .padding(.bottom,10)
+                           }
+                    
+                       }
+             
+            }
             
-            Button(action: {
-                      withAnimation {
-                          if currentPage < onboardingPages.count - 1 {
-                              currentPage += 1
-                          } else {
-                              // Handle completion or navigate to the main app
-                          }
-                      }
-                  }) {
-                      Text(currentPage < onboardingPages.count - 1 ? "التالي" : "ابدأ اللعب")
-                          .padding()
-                          .background(Color.next)
-                          .foregroundColor(.white)
-                          .cornerRadius(10)
-                  }
-              }
-          }
-      }
+        }
+    }
+    
+
   
 struct OnboardingView_Previews: PreviewProvider {
     static var previews: some View {
