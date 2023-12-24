106ca4473d252d37143aa1d3fd2490c070871747
Mahaaldeaij <Mahaaldeaij@gmail.com>
2023-12-24 15:30:25 +0300

    answerpage

---
+++ TimesPalm/View/CorrectAnswer .swift
@@ -0,0 +1,18 @@
+//
+//  CorrectAnswer .swift
+//  TimesPalm
+//
+//  Created by Maha on 11/06/1445 AH.
+//
+
+import SwiftUI
+
+struct CorrectAnswer_: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CorrectAnswer_()
+}
